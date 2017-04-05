05bfa25ed0744b63e6277a91473641b28d8e1b9a
parigutierrez <parivash.gutierrez@gmail.com>
2017-04-05 08:51:07 -0500

    se agrega boton reset, deshabilitar tabla y ninja master click fuera de tabla

--- js/main.js
+++ js/main.js
@@ -8,11 +8,39 @@ for(var i = 0;i<numero.length;i++){
 
 for(var i = 0;i<bomba.length;i++){
   bomba[i].addEventListener("click", mostrar);
+  bomba[i].addEventListener("click", fin);
 }
 
 function mostrar(event){
   this.innerHTML=this.value;
 }
 
+function clickFuera(){
+  event.stopPropagation();
+}
+tabla.addEventListener("click", clickFuera);
 
-//location.reload()
+document.addEventListener('click', seguirJugando);
+function seguirJugando(){
+  alert("Sigue jugango");
+
+}
+
+var reiniciar = document.getElementById("reset");
+reiniciar.addEventListener("click", reset);
+function reset(e){
+  e.stopPropagation();
+  location.reload()
+}
+
+function fin() {
+    setTimeout(function(){ alert("¡¡ B O M B A !! Esto ha explotado. Click en Reset para volver a jugar"); }, 300);
+    deshabilitar();
+}
+
+function deshabilitar(){
+     var botones = document.getElementsByTagName('button');
+     for(var i = 0; i< botones.length; i++){
+       botones[i].disabled = true;
+     }
+}
